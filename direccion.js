40bcc0ea2aef28c439ba4aaae1ede7679d1ae957
Your Name <you@example.com>
2021-12-01 02:25:53 -0500

    push

--- direccion.js
+++ direccion.js
@@ -1,1 +1,1 @@
-var url = 'http://proyecto_utp.test/';
+var url = 'http://localhost/';
--- view/niveles.php
+++ view/niveles.php
@@ -8,8 +8,8 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17c06fc261e%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17c06fc261e%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22276.9921875%22%20y%3D%22217.7%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="...">
-            <div class="carousel-caption d-none d-md-block">
+            <img class="d-block w-100" style="height: 70vh;margin-top: 20px;border-radius: 20px; background-repeat: no-repeat;  background-position-x:center; background-position-y:center;background-size: 100% 100%;" src="<?php echo $GLOBALS['BASE_URL'] ?>publico/img/instrucciones/instruccion.png" alt="...">
+            <!-- <div class="carousel-caption d-none d-md-block">
                 <h2>Nivel 1</h2>
                 <ol >
                     <li>Registrarse con un Nombre y seleccionar el nivel a jugar</li>
@@ -20,21 +20,18 @@
                     <li>Cada pregunta tendrá un tiempo límite para responder</li>
                     <li>Cada nivel tendrá un puntaje de calificación</li>
                 </ol>
-            </div>
+            </div> -->
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17c06fc261e%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17c06fc261e%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22276.9921875%22%20y%3D%22217.7%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h2>Nivel 2</h2>
-                <p>Eneste nivel se encontraran preguntas de inicio sencillo para no estresarlos mucho</p>
-            </div>
+            <img class="d-block w-100" style="height: 70vh;margin-top: 20px;border-radius: 20px; background-repeat: no-repeat;  background-position-x:center; background-position-y:center;background-size: 100% 100%;" src="<?php echo $GLOBALS['BASE_URL'] ?>publico/img/instrucciones/nivel1instruccion.png" alt="...">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17c06fc261e%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17c06fc261e%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22276.9921875%22%20y%3D%22217.7%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h2>Nivel 3</h2>
-                <p>Eneste nivel se encontraran preguntas de inicio sencillo para no estresarlos mucho</p>
-            </div>
+            <img class="d-block w-100" style="height: 70vh;margin-top: 20px;border-radius: 20px; background-repeat: no-repeat;  background-position-x:center; background-position-y:center;background-size: 100% 100%;" src="<?php echo $GLOBALS['BASE_URL'] ?>publico/img/instrucciones/nivel2instruccion.png" alt="...">
+
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" style="height: 70vh;margin-top: 20px;border-radius: 20px; background-repeat: no-repeat;  background-position-x:center; background-position-y:center;background-size: 100% 100%;"  src="<?php echo $GLOBALS['BASE_URL'] ?>publico/img/instrucciones/nivel3instruccion.png" alt="...">
+
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -47,6 +44,7 @@
     </a>
 </div>
 <script>
+    localStorage.setItem('background',"<?php echo $GLOBALS['BASE_URL'] ?>publico/img/img/fondojugadores.png");
     var audio = document.getElementById("audio");
     var time_audio = localStorage.getItem("audio");
     console.log("Audio= "+time_audio)
